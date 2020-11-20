e086f51a88b0c8134aa3241a3fb2d8239e9a1e69
CN\tangzhengyun <tangzhengyun@corp.netease.com>
2020-11-20 11:24:04 +0800

    login

---
+++ src/services/user.ts
@@ -0,0 +1,15 @@
+import qs from 'qs'
+import request from '@/utils/request'
+
+interface User {
+    phone: string;
+    password: string;
+}
+
+export const login = async (data: User) => {
+   return request({
+        url: '/front/user/login',
+        headers: { 'content-type': 'x-www-form-urlencoded' },
+        data: qs.stringify(data)
+    })
+}
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,11 +2,13 @@
     <div class="login">
         <el-form ref="form"
           label-position="top"
-          :model="form" label-width="80px">
-            <el-form-item label="手机号">
+          :model="form" label-width="80px"
+          :rules="rules"
+          :isLoading="isLoading">
+            <el-form-item label="手机号" prop="phone">
                 <el-input v-model="form.phone"></el-input>
             </el-form-item>
-            <el-form-item label="密码">
+            <el-form-item label="密码" prop="password">
                 <el-input v-model="form.password"></el-input>
             </el-form-item>
             <el-form-item>
@@ -17,28 +19,55 @@
 </template>
 <script lang="ts">
 import Vue from 'vue'
-import request from '@/utils/request'
-import qs from 'qs'
+import { login } from '@/services/user'
+import { Form } from 'element-ui'
 
 export default Vue.extend({
     name: 'Login',
     methods: {
       async onSubmit () {
-        // console.log('submit!')
-        const res = await request({
-            url: '/front/user/login',
-            headers: { 'content-type': 'x-www-form-urlencoded' },
-            data: qs.stringify(this.form)
-        })
-        console.log(res)
+          try {
+            await (this.$refs.form as Form).validate()
+            this.isLoading = true
+            // const { data } = await request({
+            //     url: '/front/user/login',
+            //     headers: { 'content-type': 'x-www-form-urlencoded' },
+            //     data: qs.stringify(this.form)
+            // })
+            const { data } = await login(this.form)
+            
+            if (data.state === 1) {
+                this.$message.error(data.message)
+            } else {
+                this.$store.commit('setUser', data.content)
+                this.$router.push({
+                    name: 'home'
+                })  
+                this.$message.success('登录成功')
+            }
+          } catch (e) {
+
+          }
+        this.isLoading = false
       }
     },
     data () {
         return {
             form: {
-                phone: '',
-                password: ''
-            }
+                phone: '18201288771',
+                password: '111111'
+            },
+            rules: {
+                phone: [
+                    { required: true, message: '请输入手机号', trigger: 'blur' },
+                    { pattern: /^1\d{10}$/, message: '请输入正确的手机号', trigger: 'blur' }
+                ],
+                password: [
+                    { required: true, message: '请输入密码', trigger: 'blur' },
+                    { min: 6, max: 18, message: '长度在 6 到 18 个字符', trigger: 'blur' }
+                ]
+            },
+            isLoading: false
         }
     }
 })
