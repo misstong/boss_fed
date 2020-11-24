8b1786d653374acc74a5f48b6fe921fd1fda227b
CN\tangzhengyun <tangzhengyun@corp.netease.com>
2020-11-24 16:08:37 +0800

    temp

--- src/views/advert/AddOrUpdate.vue
+++ src/views/advert/AddOrUpdate.vue
@@ -1,13 +1,61 @@
 <template>
     <el-card>
-        <el-form :inline="true" :model="form" class="demo-form-inline">
-        <el-form-item label="广告位名称">
-            <el-input v-model="form.name"></el-input>
-        </el-form-item>
-        <el-form-item>
-            <el-button type="primary" @click="onSubmit">提交</el-button>
-        </el-form-item>
-    </el-form>
+        <el-form label-position="left" label-width="80px" :model="form" class="demo-form-inline">
+            <el-form-item label="广告名称">
+                <el-input v-model="form.name"></el-input>
+            </el-form-item>
+            <el-form-item label="广告位置">
+                <el-select v-model="form.spaceId" placeholder="请选择">
+                    <el-option
+                        v-for="space in spaces"
+                        :key="space.id"
+                        :label="space.name"
+                        :value="space.id">
+                    </el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item label="开始时间">
+                 <el-date-picker
+                    v-model="form.startTime"
+                    type="date"
+                    placeholder="选择日期">
+                </el-date-picker>
+            </el-form-item>
+            <el-form-item label="到期时间">
+                <el-date-picker
+                    v-model="form.endTime"
+                    type="date"
+                    placeholder="选择日期">
+                </el-date-picker>
+            </el-form-item>
+            <el-form-item label="上线/下线">
+                <el-radio-group v-model="form.status">
+                    <el-radio :label="0">下线</el-radio>
+                    <el-radio :label="1">上线</el-radio>
+                </el-radio-group>
+            </el-form-item>
+            <el-form-item label="广告图片">
+                <el-upload
+                action="https://jsonplaceholder.typicode.com/posts/"
+                list-type="picture-card">
+                <i class="el-icon-plus"></i>
+                </el-upload>
+            </el-form-item>
+            <el-form-item label="广告链接">
+                <el-input v-model="form.link"></el-input>
+            </el-form-item>
+            <el-form-item label="广告备注">
+                <el-input
+                type="textarea"
+                :rows="2"
+                placeholder="请输入内容"
+                v-model="form.text">
+                </el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="onSubmit">提交</el-button>
+            </el-form-item>
+        </el-form>
     </el-card>
     
 </template>
@@ -20,8 +68,16 @@ export default Vue.extend({
         return {
             form: {
                 name: '',
-                id: undefined
-            }
+                id: undefined,
+                spaceId: 0,
+                startTime: '',
+                endTime: '',
+                status: 0,
+                img: '',
+                link: '',
+                text: ''
+            },
+            spaces: []
         }
     },
     props: {
